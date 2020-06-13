1b7f5ba4f4a9be2e08b036bd2f29946910c60b61
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2020-06-12 21:38:17 -0500

    Move signal handler to global util.

--- aether/util/signal_handler.cpp
+++ aether/util/signal_handler.cpp
@@ -7,7 +7,7 @@
 
 #include "signal_handler.hpp"
 
-namespace input::util {
+namespace util {
     signal_handler::signal_handler(boost::asio::io_service &ios)
         : ios(ios),
         signals(ios),
@@ -24,7 +24,7 @@ namespace input::util {
         signals.cancel();
     }
 
-    void signal_handler::wait(const proxy::callback &callback) {
+    void signal_handler::wait(const callback &callback) {
         signals.async_wait(boost::bind(&signal_handler::on_signal, this,
             boost::asio::placeholders::error, boost::asio::placeholders::signal_number, callback));
     }
@@ -37,13 +37,12 @@ namespace input::util {
         paused = false;
     }
 
-    void signal_handler::on_signal(const boost::system::error_code &error, int signo, const proxy::callback &handler) {
+    void signal_handler::on_signal(const boost::system::error_code &error, int signo, const callback &handler) {
         if (error == boost::system::errc::success) {
             if (paused) {
                 wait(handler);
             }
             else {
-                ios.stop();
                 handler();
             }
         }
--- aether/util/signal_handler.hpp
+++ aether/util/signal_handler.hpp
@@ -12,26 +12,28 @@
 #include <boost/asio.hpp>
 #include <boost/bind.hpp>
 
-#include <aether/proxy/types.hpp>
 #include <aether/util/console.hpp>
 
-namespace input::util {
+namespace util {
     /*
         Class for handling exit signals.
         Runs its own io_service and thread, separate from the server.
     */
     class signal_handler {
+    public:
+        using callback = std::function<void()>;
+
     private:
         boost::asio::io_service &ios;
         boost::asio::signal_set signals;
         bool paused;
 
-        void on_signal(const boost::system::error_code &error, int signo, const proxy::callback &handler);
+        void on_signal(const boost::system::error_code &error, int signo, const callback &handler);
 
     public:
         signal_handler(boost::asio::io_service &ios);
         ~signal_handler();
-        void wait(const proxy::callback &handler);
+        void wait(const callback &handler);
         void pause();
         void unpause();
     };
