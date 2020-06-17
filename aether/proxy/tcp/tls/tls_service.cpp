8af82f28d527b002a8146ec679e566008cf9378d
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2020-06-16 21:59:38 -0500

    Read first message in TLS service.

--- aether/proxy/tcp/tls/tls_service.cpp
+++ aether/proxy/tcp/tls/tls_service.cpp
@@ -15,6 +15,51 @@ namespace proxy::tcp::tls {
     { }
     
     void tls_service::start() {
+        read_client_hello();
+    }
 
+    void tls_service::read_client_hello() {
+        flow.client.read_async(boost::bind(&tls_service::on_read_client_hello, this,
+            boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred));
+    }
+
+    void tls_service::on_read_client_hello(const boost::system::error_code &error, std::size_t bytes_transferred) {
+        if (error != boost::system::errc::success) {
+            // There may be data in the stream that is still inteded for the server
+            // Switch to a TCP tunnel to be safe
+            handle_not_client_hello();
+        }
+        else {
+            try {
+                std::istream input = flow.client.input_stream();
+                std::size_t remaining = client_hello_reader.read(input);
+
+                // Parsing complete
+                if (remaining == 0) {
+                    handle_client_hello();
+                }
+                // Need to read again (unlikely)
+                else {
+                    read_client_hello();
+                }
+            }
+            // The data was not formatted as we would expect, but it still
+            // may be intended for the server
+            catch (const error::tls::invalid_client_hello_exception &) {
+                handle_not_client_hello();
+            }
+        }
+    }
+
+    void tls_service::handle_not_client_hello() {
+        // This is NOT a Client Hello message, so TLS is the wrong protocol to use
+        // Thus, we forward the data to a TCP tunnel
+        flow.server << client_hello_reader.get_bytes();
+        owner.switch_service<tunnel::tunnel_service>();
+    }
+
+    void tls_service::handle_client_hello() {
+        // TODO: Parse Client Hello data
+        handle_not_client_hello();
     }
 }
--- aether/proxy/tcp/tls/tls_service.hpp
+++ aether/proxy/tcp/tls/tls_service.hpp
@@ -8,6 +8,9 @@
 #pragma once
 
 #include <aether/proxy/base_service.hpp>
+#include <aether/proxy/tcp/tls/handshake/handshake_reader.hpp>
+#include <aether/proxy/tcp/tls/handshake/client_hello.hpp>
+#include <aether/proxy/tcp/tunnel/tunnel_service.hpp>
 
 namespace proxy::tcp::tls {
     /*
@@ -18,6 +21,14 @@ namespace proxy::tcp::tls {
     class tls_service
         : public base_service {
     private:
+        std::unique_ptr<handshake::client_hello> client_hello_msg;
+        handshake::handshake_reader client_hello_reader;
+
+        void read_client_hello();
+        void on_read_client_hello(const boost::system::error_code &error, std::size_t bytes_transferred);
+        void handle_client_hello();
+
+        void handle_not_client_hello();
 
     public:
         tls_service(connection::connection_flow &flow, connection_handler &owner,
