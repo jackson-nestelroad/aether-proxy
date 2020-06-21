d9c316012adc9990c2b77fa82adcdd7fccec5bba
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2020-06-20 20:35:16 -0500

    Buffer segment and constant buffer segment.

--- aether/util/buffer_segment.cpp
+++ aether/util/buffer_segment.cpp
@@ -8,58 +8,67 @@
 #include "buffer_segment.hpp"
 
 namespace util::buffer {
-    buffer_segment::buffer_segment()
+    base_segment::base_segment()
         : is_complete(false),
-        read_data(),
+        committed_data(),
+        buffer_data(),
         num_bytes_read(0)
     { }
 
-    std::string &buffer_segment::data_ref() {
-        return read_data;
+    std::string &base_segment::data_ref() {
+        return committed_data;
     }
 
-    std::string buffer_segment::export_data() const {
-        return read_data;
+    std::string base_segment::export_data() const {
+        return committed_data;
     }
 
-    bool buffer_segment::complete() const {
+    bool base_segment::complete() const {
         return is_complete;
     }
 
-    std::size_t buffer_segment::bytes_read() const {
-        return num_bytes_read;
+    std::size_t base_segment::size() const {
+        return committed_data.size();
     }
 
-    void buffer_segment::reset() {
+    std::size_t base_segment::buffer_size() const {
+        return buffer_data.size();
+    }
+
+    void base_segment::reset() {
         is_complete = false;
-        read_data.clear();
+        committed_data.clear();
+        buffer_data.clear();
+        num_bytes_read = 0;
+    }
+    
+    void base_segment::commit_buffer() {
+        std::copy(buffer_data.begin(), buffer_data.begin() + num_bytes_read, std::back_inserter(committed_data));
+        buffer_data.clear();
         num_bytes_read = 0;
     }
 
-    void buffer_segment::mark_as_complete() {
-        is_complete = true;
-    }
-
-    void buffer_segment::mark_as_incomplete() {
+    void base_segment::commit() {
+        commit_buffer();
         is_complete = false;
     }
 
-    bool buffer_segment::read_bytes(std::istream &in, std::size_t bytes) {
-        if (!is_complete) {
-            read_data.resize(bytes);
-            bool success = in.read(&read_data[num_bytes_read], bytes) && !in.eof();
-            num_bytes_read += in.gcount();
-            is_complete = success;
-        }
-        return is_complete;
+    void base_segment::mark_as_complete() {
+        is_complete = true;
+    }
+
+    void base_segment::mark_as_incomplete() {
+        is_complete = false;
     }
 
     bool buffer_segment::read_up_to_bytes(std::istream &in, std::size_t bytes) {
         if (!is_complete) {
-            read_data.resize(bytes);
-            bool success = in.read(&read_data[num_bytes_read], bytes - num_bytes_read) && !in.eof();
+            buffer_data.resize(bytes);
+            is_complete = in.read(&buffer_data[num_bytes_read], bytes - num_bytes_read) && !in.eof();
             num_bytes_read += in.gcount();
-            is_complete = success;
+            if (is_complete) {
+                commit_buffer();
+            }
         }
         return is_complete;
     }
@@ -67,10 +76,12 @@ namespace util::buffer {
     bool buffer_segment::read_until(std::istream &in, char delim) {
         if (!is_complete) {
             std::string temp;
-            bool success = std::getline(in, temp, delim) && !in.eof();
-            read_data += temp;
+            is_complete = std::getline(in, temp, delim) && !in.eof();
+            buffer_data += temp;
             num_bytes_read += temp.length();
-            is_complete = success;
+            if (is_complete) {
+                commit_buffer();
+            }
         }
         return is_complete;
     }
@@ -83,18 +94,42 @@ namespace util::buffer {
                 std::string next;
                 bool success = std::getline(in, next, final_delim) && !in.eof();
                 if (!next.empty()) {
-                    read_data += next + final_delim;
+                    buffer_data += next + final_delim;
                     num_bytes_read += next.length() + 1;
                 }
-                // If not successful, not evne the last character of the delimiter was found
+                // If not successful, not even the last character of the delimiter was found
                 if (!success) {
                     return false;
                 }
-            } while ((num_bytes_read < delim_size) || (read_data.substr(num_bytes_read - delim_size, num_bytes_read) != delim));
+            } while ((num_bytes_read < delim_size) || (buffer_data.substr(num_bytes_read - delim_size, num_bytes_read) != delim));
             // Remove delimiter
-            read_data = read_data.substr(0, num_bytes_read - delim_size);
+            buffer_data = buffer_data.substr(0, num_bytes_read - delim_size);
             num_bytes_read -= delim_size;
             is_complete = !in.fail() && !in.eof();
+            if (is_complete) {
+                commit_buffer();
+            }
+        }
+        return is_complete;
+    }
+
+    bool const_buffer_segment::read_up_to_bytes(proxy::const_streambuf &buf, std::size_t bytes, std::size_t size) {
+        if (!is_complete) {
+            buffer_data.resize(bytes);
+            std::size_t offset = committed_data.size();
+            std::size_t bytes_available = size - offset;
+            std::size_t to_read = bytes - num_bytes_read;
+            if (bytes_available >= to_read) {
+                std::copy(boost::asio::buffers_begin(buf) + offset + num_bytes_read, boost::asio::buffers_begin(buf) + offset + to_read, &buffer_data[num_bytes_read]);
+                num_bytes_read += to_read;
+                is_complete = true;
+                commit_buffer();
+            }
+            else {
+                std::copy(boost::asio::buffers_begin(buf) + offset + num_bytes_read, boost::asio::buffers_begin(buf) + size, &buffer_data[num_bytes_read]);
+                num_bytes_read += bytes_available;
+                is_complete = false;
+            }
         }
         return is_complete;
     }
--- aether/util/buffer_segment.hpp
+++ aether/util/buffer_segment.hpp
@@ -12,22 +12,29 @@
 #include <string_view>
 #include <iterator>
 
+#include <aether/proxy/types.hpp>
+
 namespace util::buffer {
     /*
-        Utility class for managing data that may need to be read from an
-            input stream multiple times to meet the complete condition.
+        Base attributes and methods for managing data that may be collected
+            from an input buffer or stream in one or more method calls.
     */
-    class buffer_segment {
-    private:
+    class base_segment {
+    protected:
         // Data cannot be read if the segment is marked as complete
         bool is_complete;
 
-        std::string read_data;
+        std::string committed_data;
+
+        std::string buffer_data;
         std::size_t num_bytes_read;
 
-    public:
-        buffer_segment();
+        base_segment();
 
+        // Moves buffer_data to committed_data
+        void commit_buffer();
+
+    public:
         /*
             Returns a reference to the data owned by the segment.
         */
@@ -46,34 +53,46 @@ namespace util::buffer {
         bool complete() const;
 
         /*
-            Returns the number of bytes read into the buffer segment.
+            Returns the number of bytes committed to the buffer segment.
         */
-        std::size_t bytes_read() const;
+        std::size_t size() const;
+
+        /*
+            Returns the number of bytes in the buffer (non-committed data).
+        */
+        std::size_t buffer_size() const;
 
         /*
             Resets all data and flags.
         */
         void reset();
 
-        void mark_as_complete();
+        /*
+            Commits all currently buffered data to be exported and marks the segment
+                as complete.
+        */
+        void commit();
+
         void mark_as_incomplete();
+        void mark_as_complete();
 
         /*
             Copies the segment data to an iterator location.
         */
         template <typename Iterator>
         void copy_data(const Iterator &dest) const {
-            std::copy(read_data.begin(), read_data.end(), dest);
+            std::copy(committed_data.begin(), committed_data.end(), dest);
         }
+    };
 
-        /*
-            Reads a given number of bytes from the stream.
-            Marked as complete if the exact number of bytes are read
-                in this read session. This method is not stateful,
-                so it will not account for the results of previous reads.
-        */
-        bool read_bytes(std::istream &in, std::size_t bytes);
-
+    /*
+        Utility class for managing data that may need to be read from an
+            input stream multiple times to meet the complete condition.
+        Permanently removes data from the stream.
+    */
+    class buffer_segment : 
+        public base_segment {
+    public:
         /*
             Reads from the stream until the total number of bytes read
                 matches the number passed to this method.
@@ -92,5 +111,23 @@ namespace util::buffer {
             Reads from the stream until a multi-character delimiter is found.
         */
         bool read_until(std::istream &in, std::string_view delim);
+    };
+
+    /*
+        Utility class for managing data that may need to be read from a
+            constant buffer multiple times to meet the complete condition.
+        Does not remove data when reading.
+    */
+    class const_buffer_segment 
+        : public base_segment {
+    public:
+        /*
+            Reads from the stream until the total number of bytes in the buffer
+                matches the number passed to this method.
+            This method is stateful, so it will account for the results
+                of previous reads.
+            This method will fail if bytes_read is greater than bytes.
+        */
+        bool read_up_to_bytes(proxy::const_streambuf &buf, std::size_t bytes, std::size_t size);
     };
 }
