a40dccad185ebdd1e2ed618afd83d4d5e1f7dc45
Jackson Nestelroad <jackson@nestelroad.com>
2023-02-04 12:22:47 -0800

    Generate functions for non-exception error types

---
+++ src/aether/proxy/error/error.hpp
@@ -0,0 +1,48 @@
+/*********************************************
+
+  Copyright (c) Jackson Nestelroad 2023
+  jackson.nestelroad.com
+
+*********************************************/
+
+#pragma once
+
+#include <string>
+#include <utility>
+
+#include "aether/proxy/error/error_code.hpp"
+#include "aether/proxy/error/error_state.hpp"
+#include "aether/util/result.hpp"
+
+namespace proxy {
+
+template <typename T>
+using result = util::result<T, error::error_state>;
+
+namespace error {
+
+#define GENERATE_ALL_FUNCTIONS_false(name, macro_name, msg, foreach) macro_name##_ERRORS(foreach, name)
+
+#define GENERATE_ALL_FUNCTIONS_true(name, macro_name, msg, foreach) \
+  namespace name {                                                  \
+  macro_name##_ERRORS(foreach, name)                                \
+  }
+
+#define GENERATE_ALL_FUNCTIONS(num, name, macro_name, msg, nested, foreach) \
+  GENERATE_ALL_FUNCTIONS_##nested(name, macro_name, msg, foreach)
+
+#define GENERATE_FUNCTION(num, name, msg, base)                                       \
+  error_state name##_error(std::string message) {                                     \
+    return proxy_error(errc::name, #base "::" #name "_error: " + std::move(message)); \
+  }
+
+ERROR_CATEGORIES(GENERATE_ALL_FUNCTIONS, GENERATE_FUNCTION)
+
+#undef GENERATE_FUNCTION
+#undef GENERATE_ALL_FUNCTIONS
+#undef GENERATE_ALL_FUNCTIONS_true
+#undef GENERATE_ALL_FUNCTIONS_false
+
+}  // namespace error
+
+}  // namespace proxy
--- src/aether/proxy/error/error_state.cpp
+++ src/aether/proxy/error/error_state.cpp
@@ -37,6 +37,20 @@ std::string error_state::get_message_or_boost() const noexcept {
   return message_.empty() ? boost_error_code_.message() : message_;
 }
 
+error_state boost_error(boost::system::error_code code, std::string message) {
+  error_state err;
+  err.set_boost_error(code);
+  err.set_message(std::move(message));
+  return err;
+}
+
+error_state proxy_error(error_code code, std::string message) {
+  error_state err;
+  err.set_proxy_error(code);
+  err.set_message(std::move(message));
+  return err;
+}
+
 std::ostream& operator<<(std::ostream& out, const error_state& error) {
   // Error message overrides anything else stored.
   if (error.has_message()) {
